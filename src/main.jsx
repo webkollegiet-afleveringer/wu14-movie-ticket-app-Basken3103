de7ccfca77d9719a8d3ea4f11740eec465dc5bef
Christopher S. Vesterby <vesterby@hotmail.dk>
2026-03-11 14:02:12 +0100

    Er i gang med at sætte Routing op

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDom from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDom.createRoot(root).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 )
