d5c7c227b987d99d096e51170e5297b47650bae4
Christopher S. Vesterby <vesterby@hotmail.dk>
2026-03-22 14:58:34 +0100

    Er i gang med Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,34 @@
+import { useEffect, useState } from "react";
+import "../style/Home.scss";
+
 function Home() {
+  const [comingSoonMovies, setComingSoonMovies] = useState([null]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const token = import.meta.env.VITE_TMDB_TOKEN
+  const baseImgUrl = "https://image.tmdb.org/t/p/w500";
+
+  const cinemas = [
+    {
+      id: 1,
+      name: "Viva Cinema",
+      distance: "5,2 Kilometers",
+      closingTime: "Closed 10.00 PM",
+      rating: "4,9",
+      image:
+        "https://via.placeholder.com/80x80/ffffff/cc0000?text=VIVA",
+    },
+      {
+      id: 2,
+      name: "EbonyLife Cinema",
+      distance: "6,5 Kilometers",
+      closingTime: "Closed 09.00 PM",
+      rating: "5,0",
+      image:
+        "https://via.placeholder.com/80x80/111111/ffffff?text=EL",
+    },
+  ];
 
   return (
     <>
