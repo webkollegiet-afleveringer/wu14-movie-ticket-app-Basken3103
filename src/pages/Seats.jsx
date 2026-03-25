251f9cec4ec2df756b36cdd7d853bc1860df984b
Christopher S. Vesterby <vesterby@hotmail.dk>
2026-03-25 13:56:58 +0100

    Har nu fået færdiggørt den første del af html delen af Seats.jsx

--- src/pages/Seats.jsx
+++ src/pages/Seats.jsx
@@ -84,7 +84,69 @@ function Seats() {
     <section className="seats">
       <header className="seats__header">
         <button className="seats__back-button">←</button>
+        <h1 className="seats__title">Select Seats</h1>
+        <div className="seats__spacer"></div>
       </header>
+
+      <div className="seats__form-group">
+        <label className="seats__label">Cinema</label>
+        <div className="seats__select-wrapper seats__select-wrapper--full">
+          <select
+          className="seats__select"
+          value={selectedCinema}
+          onChange={(e) => setSelectedCinema(e.target.value)}
+          >
+            <option>Empire XXI Yogyakarta</option>
+            <option>Viva Cinema</option>
+            <option>EbonyLife Cinema</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="seats__row-fields">
+        <div className="seats__form-group seats__form-group--half">
+          <label className="seats__label">Date</label>
+          <div className="seats__select-wrapper">
+            <select
+             className="seats__select"
+             value={selectedDate}
+             onChange={(e) => setSelectedDate(e.target.value)}
+            
+            >
+              <option>02 Nov 2021</option>
+              <option>03 Nov 2021</option>
+              <option>04 Nov 2021</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="seats__form-group seats__form-group--half">
+          <label className="seats__label">Time</label>
+          <div className="seats__select-wrapper">
+            <select
+              className="seats__select"
+              value={selectedTime}
+              onChange={(e) => setSelectedTime(e.target.value)}
+            
+            >
+              <option>01.00 PM</option>
+              <option>04.00 PM</option>
+              <option>08.00 PM</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="screen">
+        <div className="screen__arc"></div>
+        <div className="screen__glow"></div>
+      </div>
+
+
+        
+
+      </div>
+
+
 
      
       
