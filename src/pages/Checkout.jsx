f96186bb246a291a02418d8531df76643a6c2621
Christopher S. Vesterby <vesterby@hotmail.dk>
2026-04-08 11:26:04 +0200

    Er gået i gang med Checkout.jsx, da jeg synes det giver mere mening at arbejde på design derhjemme. + jeg har lavet en designguide som skal printes ud.

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,13 @@
+import Header from "../components/Header";
+
 function Checkout() {
 
   return (
-    <>
-
+    <section>
+      
      <h1>Checkout</h1>
       
-    </>
+    </section>
   )
 }
 
