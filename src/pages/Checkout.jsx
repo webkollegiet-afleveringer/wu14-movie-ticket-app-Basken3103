e71f7b0a4194769f27984da77ab8a7b090583a14
Christopher S. Vesterby <vesterby@hotmail.dk>
2026-04-08 14:16:17 +0200

    Er næsten færdig med Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,9 +3,81 @@ import Header from "../components/Header";
 function Checkout() {
 
   return (
-    <section>
+    <section className="checkout">
+      <Header title="Checkout" />
+
+      {/* Payment Method*/}
+      <div className="checkout__section">
+        <div className="checkout__section-header">
+          <h2 className="checkout__title">Payment Method</h2>
+          <span className="checkout__change">Change</span>
+        </div>
+
+        <div className="checkout__card">
+          <div className="checkout__card-top">
+            <div className="checkout__card-icons">
+              <span className="checkout__circle checkout__circle --red"></span>
+              <span className="checkout__circle checkout__circle --yellow"></span>
+            </div>
+
+            <div className="checkout__balance">
+                <p className="checkout__balance-label">Balance</p>
+                <h3 className="checkout__balance-value">$120,580,00</h3>
+            </div>
+          </div>
+
+          <div className="checkout__card-bottom">
+            <div>
+              <p className="checkout__label">Card Holder</p>
+              <p className="checkout__value">Miles Morales</p>
+            </div>
+
+            <p className="checkout__card-number">
+               **** **** **** 51446
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Payment Details */}
+      <div className="checkout__section">
+        <h2 className="checkout__title">Payment Details</h2>
+
+        <div className="checkout__field">
+          <label className="checkout__label">Your Email</label>
+          <input type="email" defaultValue="Milesmorales@gmail.com" className="checkout__input"></input>
+        </div>
+
+        <div className="checkout__field">
+          <label className="checkout__label">Cardholder Name</label>
+          <input type="text" defaultValue="Miles Morales" className="checkout__input"></input>
+        </div>
+
+        {/* Nedereste input felt virker ikke, skal have fundet ud af hvorfor */}
+        <div className="checkout__field">
+          <label className="checkout__label">Card Number</label>
+          <input type="text" defaultValue="**** **** **** 51446" className="checkout__input"></input>
+        </div>
+
+        <div className="checkout__row">
+          <div className="checkout__field">
+            <label className="checkout__label">Date</label>
+            <div className="checkout__input-wrapper">
+              <input type="text" defaultValue= "02 Nov 2021" className="checkout__input"></input>
+              <span className="checkout__arrow">▼</span>
+
+            </div>
+          </div>
+        </div>
+
+
+
+
+      </div>
+
+
       
-     <h1>Checkout</h1>
+     
       
     </section>
   )
